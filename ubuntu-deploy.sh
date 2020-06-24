46baeb4be840c6d65ce7852ff3db4f815e817d3d
lubos klokner <lubos@klokner.sk>
2020-06-24 08:44:38 +0200

    reorder scripts for zsh tuning

--- ubuntu-deploy.sh
+++ ubuntu-deploy.sh
@@ -28,13 +28,16 @@ if [ "${amIRoot}" == "root" ]; then
 fi
 
 echo "Shell..."
+echo "Installing Oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "Installing powerlevel10k theme..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
 echo "Installing plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "Installing Oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "Changing the default shell to zsh..."
 chsh /usr/bin/zsh
 
