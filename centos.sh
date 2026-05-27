91c8b94bda24899d03117a0462dd0073e25e86de
lubos klokner <lubos@klokner.sk>
2026-05-27 16:41:15 +0200

    deleted empty file

--- centos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-dnf -y install zsh git wget vim unzip python38
-
-sudo alternatives --set python /usr/bin/python3
-
-git clone git://github.com/wting/autojump.git
-cd autojump
-./install.py
